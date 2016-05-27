7098a18e97a87b22069c68ea18ea1880853a62cb
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-05-28 00:20:27 +0200

    simple config

---
+++ tests/Config/SimpleConfigUnitTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace GraphAware\Reco4PHP\Tests\Config;
+
+use GraphAware\Reco4PHP\Config\SimpleConfig;
+use GraphAware\Reco4PHP\Tests\Helper\FakeNode;
+use GraphAware\Bolt\Result\Type\Node;
+
+class SimpleConfigUnitTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefault()
+    {
+        $config = new SimpleConfig();
+        $this->assertEquals(PHP_INT_MAX, $config->limit());
+        $this->assertEquals(PHP_INT_MAX, $config->maxTime());
+        $this->assertFalse($config->containsKey('key'));
+    }
+
+    public function testCustomLimitAndTime()
+    {
+        $config = new SimpleConfig(100, 1000);
+        $this->assertEquals(100, $config->limit());
+        $this->assertEquals(1000, $config->maxTime());
+    }
+
+    public function testConfigExtendsKeyValue()
+    {
+        $config = new SimpleConfig();
+        $config->add('month', 'june');
+        $config->add('obj', FakeNode::createDummy());
+        $this->assertEquals('june', $config->get('month'));
+        $this->assertInstanceOf(Node::class, $config->get('obj'));
+    }
+}
---
+++ tests/Context/ContextUnitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GraphAware\Reco4PHP\Tests\Context;
+
+use GraphAware\Reco4PHP\Context\SimpleContext;
+use GraphAware\Reco4PHP\Tests\Helper\FakeNode;
+use GraphAware\Reco4PHP\Config\Config;
+
+class ContextUnitTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefault()
+    {
+        $input = FakeNode::createDummy();
+        $context = new SimpleContext($input);
+        $this->assertInstanceOf(Config::class, $context->config());
+        $this->assertEquals($input, $context->input());
+    }
+}
+
