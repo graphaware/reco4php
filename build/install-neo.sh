9e46d6b0bbf183ff0b40118db46a6116ce577e5e
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-06-13 11:19:08 +0200

    make sure data is in

--- build/install-neo.sh
+++ build/install-neo.sh
@@ -8,4 +8,5 @@ mkdir neo
 tar xzf neo4j-enterprise-3.0.2-unix.tar.gz -C neo --strip-components=1 > null
 sed -i.bak '/\(dbms\.security\.auth_enabled=\).*/s/^#//g' ./neo/conf/neo4j.conf
 cp -R build/graph.db neo/data/databases/
+ls -l neo/data/databases/graph.db
 neo/bin/neo4j start > null &
